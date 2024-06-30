326eacb11a41e9a526df8d4ae24a9d454f47ee0a
落合謙次 <kenotiai@gmail.com>
2024-06-30 11:07:53 +0900

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-const webpack = require('webpack');
-
 module.exports = {
   //  ビルドしたときのパス 空文字にして相対パスにする。
   publicPath: process.env.NODE_ENV === 'production' ? '' : '',
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+    }
+  }
 }
