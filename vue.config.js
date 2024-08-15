637237f3448e4defe7e8d397c1986dc969a9ba05
kenzkenz <kenotiai@gmail.com>
2024-08-15 21:07:46 +0900

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   transpileDependencies: [
-    // "ol",
-    // "color-parse",
+    "ol",
+    "color-parse",
     // "@cieloazul310/ol-gsi-vt",
     "@cieloazul310/ol-gsi-vt-style",
     "@cieloazul310/ol-gsi-vt-style-utils"
